fbcf8590223fd083bd68c2f1ac35d17ce4db50a0
tiwamit4 <128207703+tiwamit4@users.noreply.github.com>
2024-10-15 10:09:44 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,36 +1,37 @@
-import streamlit as st
+import streamlit as st 
 import numpy as np
-import tensorflow as tf
-from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler,LabelEncoder,OneHotEncoder
 import pickle
 
-# Load the trained model
+#Load the Trained model
 model = tf.keras.models.load_model('model.h5')
 
-# Load the encoders and scaler
-with open('label_encoder_gender.pkl', 'rb') as file:
-    label_encoder_gender = pickle.load(file)
 
-with open('onehot_encoder_geo.pkl', 'rb') as file:
-    onehot_encoder_geo = pickle.load(file)
+### Load encoder and scaler
+with open('onehot_encoder_geo.pkl','rb') as file:
+    onehot_encoder_geo=pickle.load(file)
+    
+with open('label_encoder_gender.pkl','rb') as file:
+    label_encoder_gender=pickle.load(file)
 
-with open('scaler.pkl', 'rb') as file:
-    scaler = pickle.load(file)
+with open('scaler.pkl','rb') as file:
+    scaler=pickle.load(file)
+    
 
-
-## streamlit app
-st.title('Customer Churn PRediction')
+## Streamlit app
+st.title('Customer Churn Prediction')
 
 # User input
-geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
-gender = st.selectbox('Gender', label_encoder_gender.classes_)
-age = st.slider('Age', 18, 92)
-balance = st.number_input('Balance')
+geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0]) #Dropdown
+gender = st.selectbox('Gender', label_encoder_gender.classes_) 
+age = st.slider('Age', 18, 92) #Slider
+balance = st.number_input('Balance') 
 credit_score = st.number_input('Credit Score')
 estimated_salary = st.number_input('Estimated Salary')
-tenure = st.slider('Tenure', 0, 10)
-num_of_products = st.slider('Number of Products', 1, 4)
+tenure = st.slider('Tenure', 0, 10) #Slider
+num_of_products = st.slider('Number of Products', 1, 4) #Slider
 has_cr_card = st.selectbox('Has Credit Card', [0, 1])
 is_active_member = st.selectbox('Is Active Member', [0, 1])
 
@@ -56,7 +57,6 @@ input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis
 
 # Scale the input data
 input_data_scaled = scaler.transform(input_data)
-
 
 # Predict churn
 prediction = model.predict(input_data_scaled)
